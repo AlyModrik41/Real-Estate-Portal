ab450efda6f9e24e98439bd5c31b98696ce3d88d
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2025-05-10 20:58:50 +0300

    modryk code

--- Admin/properties.h
+++ Admin/properties.h
@@ -8,29 +8,37 @@
 using namespace std;
 
 
+
 class properties {
-int price;
-bool status;
+  int price = 0, area = 0, rooms = 0;
+  bool status = false, furniture = false;
+  string owner = "";
+  string location = "";
+  bool approved;
   bool highlighted;
-string owner;
-  string location;
 public:
   properties();
   properties(int);
+  properties(int, int, int, bool, string, string);
   ~properties();
   void set_price(int);
   void set_status(bool);
   void set_owner(string);
   void set_location(string);
-  void set_highlight(bool );
+  void set_area(int);
+  void set_rooms(int);
+  void set_furniture(bool);
+  void set_approval(bool);
+  void set_highlight(bool H);
+
   int get_price() const;
-  bool get_status() const;
   bool get_highlight() const;
+  bool get_status() const;
   string get_owner() const;
   string get_location() const;
-
+  bool get_furniture();
+  int get_area();
+  int get_rooms();
+  bool get_approval();
 };
-
-
-
 #endif //PROPERTIES_H
